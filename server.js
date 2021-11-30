271069b147419964793760f8187688ca7303f055
Kehan <likehan217@gmail.com>
2021-11-30 21:02:20 +0800

    set up server in server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Hello world")
+//----------------------------------------
+// imports
+//----------------------------------------
+const app = require("./controller/app");
+
+//----------------------------------------
+// config
+//----------------------------------------
+var port = 3000;
+var hostname="localhost";
+
+//----------------------------------------
+// main
+//----------------------------------------
+
+app.listen(port, hostname, () => {
+    console.log(`Server started and accessible via http://${hostname}:${port}/`);
+  });
