5dd8500dad30d3fc9c9892bc9489a4ad32d31a7e
Kehan <likehan217@gmail.com>
2021-11-30 13:35:27 +0800

    add in env

--- model/databaseConfig.js
+++ model/databaseConfig.js
@@ -2,6 +2,7 @@
 // imports
 //----------------------------------------
 const mysql = require("mysql");
+require("dotenv").config();
 
 //----------------------------------------
 // main code implementations
@@ -10,11 +11,11 @@ var dbconnect = {
     getConnection: function () {
     // here just build connection, but haven't yet start the connection
       var conn = mysql.createConnection({
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: 'lkH2004217200413!', //your own password
-        database: 'friendbook',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS, //your own password
+        database: 'assignment',
         dateStrings: true
       });
   
