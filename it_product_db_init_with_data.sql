5963f4d7f9efb6a02f2ea967389159b8b55c1a25
kKar <karlok.yam.1503@gmail.com>
2021-12-04 01:13:28 +0800

    Altered SQL query for User.userid with AUTO_INCREMENT

--- it_product_db_init_with_data.sql
+++ it_product_db_init_with_data.sql
@@ -143,7 +143,7 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `userid` int NOT NULL,
+  `userid` int NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `contact` int NOT NULL,
