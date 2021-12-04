6e766f5055f38eb4748358e534455b46992b2757
Kehan <likehan217@gmail.com>
2021-12-04 19:23:52 +0800

    edit the api 1,3 res and add in api 4

--- model/user.js
+++ model/user.js
@@ -17,14 +17,10 @@ const db = require("./databaseConfig.js");
 let User = {
     insert: function(user, callback) {
         var dbConn = db.getConnection();
-
         dbConn.connect(function (err) {
             if (err) {
-                console.log(err);
                 return callback(err, null);
-            } 
-
-            else {
+            }else {
                 const sql = `
                            INSERT INTO
                                 user (username, email, contact, password, type, profile_pic_url)
@@ -32,25 +28,12 @@ let User = {
                                 (?, ?, ?, ?, ?, ?) 
                            `;
 
-                dbConn.query(sql, [user.d_username, user.d_email, user.d_contact, user.d_pass, user.d_type, user.d_picUrl], (error, q_result) => {
+                dbConn.query(sql, [user.username, user.email, user.contact, user.pass, user.type, user.picUrl], (error, result) => {
                     dbConn.end();
-
                     if (error) {
-                        console.log("query error");
                         return callback(error, null);              
                     };
-                    
-                    return callback(null, q_result);
-                    
-                    /*
-                        q_result: {
-                            affectedRows: 1,
-                            ...,
-                            insertId: 5,
-                            ...,
-                        }
-                    */
-                    
+                    return callback(null, result);
                 });
             }
         });
@@ -65,7 +48,7 @@ let User = {
             }
             else {
                 console.log("Connection established!");
-                var sql = 'SELECT userid, username, email, contact, type, profile_pic_url, created_at FROM user';
+                var sql = 'SELECT userid, username, email, contact, type, profile_pic_url, createat FROM user';
                 conn.query(sql, function (err, result) {
                     conn.end();
                     if (err) {
@@ -84,18 +67,44 @@ let User = {
             if (err) {
                 return callback(err, null);
             } else {
-                const sql = "SELECT userid, username, email, contact, type, profile_pic_url, created_at FROM user WHERE userid = ?";;
-                dbConn.query(sql, [userID], (error, results) => {
+                const sql = "SELECT userid, username, email, contact, type, profile_pic_url, created_at FROM user WHERE userid = ?";
+                dbConn.query(sql, [userID], (error, result) => {
                     dbConn.end();
                     if (error) {
                         return callback(error, null);              
                     };
-                    // no record
-                    if(results.length == 0) {
-                        return callback(null, null);
-                    }else {
-                        return callback(null, results[0]); // only return the result not whole array
-                    }
+                    return callback(null, result);
+                });
+            }
+        });
+    },
+
+    edit: function (userID, user, callback) {
+        var dbConn = db.getConnection();
+        dbConn.connect(function (err) {
+            if (err) {
+                return callback(err, null);
+            } else {
+                const sql = `
+                            UPDATE
+                                user
+                            SET
+                                username = ?, 
+                                email = ?, 
+                                contact = ?, 
+                                password = ?,
+                                type = ?, 
+                                profile_pic_url = ?
+                            WHERE
+                                userid = ?
+                            `;
+
+                dbConn.query(sql, [user.username, user.email, user.contact ,user.pass, user.type, user.picUrl, userID], (error, result) => {
+                    dbConn.end();
+                    if (error) {
+                        return callback(error, null);              
+                    };
+                    return callback(null, result);
                 });
             }
         });
@@ -106,3 +115,5 @@ let User = {
 // Module Export
 //----------------------------------------
 module.exports = User;
+
+
