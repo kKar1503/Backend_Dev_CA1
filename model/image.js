80caeb32327ad881006793d26db031f7cb111f0a
kKar <karlok.yam.1503@gmail.com>
2021-12-17 15:17:23 +0800

    Added image.js

---
+++ model/image.js
@@ -0,0 +1,24 @@
+//----------------------------------------
+// BackEnd Web Development Assignment 1
+// Students:        Li Kehan & Yam Kar Lok
+// Admission No:    P2111575 & P2123181
+// Class:           DIT/FT/1B/04
+// Filename:        image.js
+//----------------------------------------
+
+//----------------------------------------
+// Imports
+//----------------------------------------
+const db = require("./databaseConfig.js");
+
+//----------------------------------------
+// Main Code Implementations
+//----------------------------------------
+let Image = {
+    
+};
+
+//----------------------------------------
+// Module Export
+//----------------------------------------
+module.exports = Image;
