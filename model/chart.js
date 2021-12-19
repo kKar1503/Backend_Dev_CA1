17b8860700a990e61cef23063b53e32070f2de4c
Kehan <likehan217@gmail.com>
2021-12-19 16:18:54 +0800

    add in endpoint for get pie chart

--- model/chart.js
+++ model/chart.js
@@ -102,34 +102,34 @@ let Chart = {
 		});
 	},
 
-	addCat: function (cat, callback) {
-		var conn = db.getConnection();
-		conn.connect(function (err) {
-			if (err) {
-				return callback(err, null);
-			} else {
-				console.log("Connection established!");
-				const sql = `
-                           INSERT INTO
-                                category(category, description)
-                           VALUES
-                                (?, ?) 
-                           `;
-				conn.query(
-					sql,
-					[cat.category, cat.description],
-					(error, result) => {
-						conn.end();
-						if (error) {
-							return callback(error, null);
-						} else {
-							return callback(null, result);
-						}
-					}
-				);
-			}
-		});
-	},
+	// addCat: function (cat, callback) {
+	// 	var conn = db.getConnection();
+	// 	conn.connect(function (err) {
+	// 		if (err) {
+	// 			return callback(err, null);
+	// 		} else {
+	// 			console.log("Connection established!");
+	// 			const sql = `
+    //                        INSERT INTO
+    //                             category(category, description)
+    //                        VALUES
+    //                             (?, ?) 
+    //                        `;
+	// 			conn.query(
+	// 				sql,
+	// 				[cat.category, cat.description],
+	// 				(error, result) => {
+	// 					conn.end();
+	// 					if (error) {
+	// 						return callback(error, null);
+	// 					} else {
+	// 						return callback(null, result);
+	// 					}
+	// 				}
+	// 			);
+	// 		}
+	// 	});
+	// },
 };
 
 //----------------------------------------
